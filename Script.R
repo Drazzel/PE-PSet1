41a967be15c95112056657ed08852e219ae61a6c
Drazzel Feliu <drazzelfeliu@Drazzels-MacBook-Pro.local>
2018-10-07 22:24:48 -0500

    Partial answer to question 1

--- Script.R
+++ Script.R
@@ -9,17 +9,7 @@ library(haven)
 # load data set
 data <- read_dta("SP_dataset.dta")
 
-# subset data by variable value
-test <- subset(data, cluster==1)
-test$clust_pair
+# question 1
 
-# count number of observations in the cluster variable
-length(which(data$cluster==1))
 
-# count number of observations
-length(test$clust_pair)
-
-test2 <- subset(data, clust_pair==47)
-test2$cluster
-length(test2$cluster)
-
+test3 <- count(data, clust_pair, treatment, cluster)

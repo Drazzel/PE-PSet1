8f5b47d02208dea25152688d3fc47ed4722d6555
Drazzel Feliu <drazzelfeliu@Drazzels-MacBook-Pro.local>
2018-10-07 20:59:22 -0500

    Overview of assignment. Working dataset. Project script.

---
+++ Script.R
@@ -0,0 +1,25 @@
+# clear all objects
+rm(list=ls())
+
+# load relevant packages
+library(tidyverse)
+library(magrittr)
+library(haven)
+
+# load data set
+data <- read_dta("SP_dataset.dta")
+
+# subset data by variable value
+test <- subset(data, cluster==1)
+test$clust_pair
+
+# count number of observations in a variable
+length(which(data$cluster==1))
+
+# count number of observations
+length(test$clust_pair)
+
+test2 <- subset(data, clust_pair==47)
+test2$cluster
+length(test2$cluster)
+
